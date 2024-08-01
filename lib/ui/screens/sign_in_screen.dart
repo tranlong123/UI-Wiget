40e7a422b7cb3fb20b88a0b2f0fc7e5e1ad85911
Tran Long <tranlonghazzi@gmail.com>
2024-08-01 13:44:05 +0700

    Task 2

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:new_app/ui/screens/footer/sign_in_footer.dart';
+import 'package:new_app/ui/screens/body/sign_in_body.dart';
 import 'package:new_app/ui/styles/colors.dart';
 
 
@@ -12,9 +12,14 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  // ignore: unused_field, prefer_final_fields
+  // ignore: unused_field
   String _email = '';
 
+  void _updateEmail(String email) {
+    setState(() {
+      _email = email;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,22 +30,20 @@ class _SignInScreenState extends State<SignInScreen> {
     ));
 final Map<String, String>? args =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
-    // ignore: unused_local_variable
     final String? emailRegister = args?['emailRegister'] ;
-    // ignore: unused_local_variable
     final String? pass = args?['pass'];
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: bgColor,
         body: SingleChildScrollView(
           child: Column(
             children: [
               // SignInHeader(email: _email),
-              // SignInBody(
-              //     onEmailChanged: _updateEmail,
-              //     emailRegister: emailRegister,
-              //     pass: pass),
-              SignInFooter()
+              SignInBody(
+                  onEmailChanged: _updateEmail,
+                  emailRegister: emailRegister,
+                  pass: pass),
+              // const SignInFooter()
             ],
           ),
         ),
